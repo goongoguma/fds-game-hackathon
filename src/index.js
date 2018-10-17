05b35cb5d3579dc6413f7d1b5ed0cd3eec9e8db3
goongoguma <wogus7an@gmail.com>
2018-10-17 16:16:09 +0900

    BINGO

--- src/index.js
+++ src/index.js
@@ -1,5 +1,121 @@
-import "./index.scss";
+let boardState = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
 
-document.querySelector("h1").addEventListener("click", e => {
-  alert("Hello World!");
+function drawBoard() {
+  document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+    rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+      if (boardState[rowIndex][colIndex] === 1) {
+        colEl.classList.add("checked");
+      } else {
+        colEl.classList.remove("checked");
+      }
+    });
+  });
+
+  if (bingo(boardState)) {
+    document.querySelector(".result").textContent = "BINGO!";
+    document.querySelector(".reset").classList.add("show");
+  } else {
+    document.querySelector(".result").textContent = "";
+    document.querySelector(".reset").classList.remove("show");
+  }
+}
+
+function bingo(arr) {
+  // 가로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[i][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  // 세로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][i] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    // 대각선 확인 (루프)
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    // 반대쪽 대각선 확인 (루프)
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][4 - j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  return false;
+}
+document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+  rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+    colEl.addEventListener("click", e => {
+      if (!bingo(boardState)) {
+        boardState[rowIndex][colIndex] = 1;
+        drawBoard();
+      }
+    });
+  });
 });
+
+// 다시 시작버튼
+// const restart = document.querySelector(".restart");
+// restart.addEventListener("click", e => {
+//   document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+//     rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+//       colEl.classList.remove("checked");
+//       boardState[rowIndex][colIndex] = 0;
+//       document.querySelector(".result").textContent = "";
+//     });
+//   });
+// });
+
+// 다시 시작버튼 강사님 풀이
+document.querySelector(".reset").addEventListener("click", e => {
+  boardState = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ];
+  drawBoard();
+});
+
+drawBoard();
